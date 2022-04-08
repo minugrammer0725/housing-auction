618103374d9dd30c25d5e9338e2351f5f3694cda
minugrammer0725 <kwawkminoo@gmail.com>
2022-04-07 17:02:49 -0700

    Landlord Contact Page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -90,7 +90,7 @@ const Listing = () => {
         {/* TODO: Map  */}
 
         {auth.currentUser?.uid !== listing.userRef && (
-          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton'>
+          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
             Contact Landlord
           </Link>
             
